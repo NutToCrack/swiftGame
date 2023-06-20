a2c3403b3fdd1dbe67e5d3cbc4283eeca72f7323
NutToCrack <86835602+NutToCrack@users.noreply.github.com>
2023-06-20 17:31:10 +0200

    adds moving curve

--- GameScene.swift
+++ GameScene.swift
@@ -7,25 +7,28 @@
 
 import SpriteKit
 import GameplayKit
-
-
+ 
 
 class GameScene: SKScene, ButtonDelegate, SKPhysicsContactDelegate {
-    
+    var goUp = false
     let ballCategory: UInt32 = 0x1 << 0
     let wallCategory: UInt32 = 0x1 << 1
     let spikeCategory: UInt32 = 0x1 << 2
     let ball = SKSpriteNode(imageNamed: "Volleyball")
+    var spikes: [SKShapeNode] = []
+    var speedFactor = 0.0
+    var sequenceFinished = false
    
     let leftButton = Button(texture: nil, color: .magenta, size: CGSize(width: 100, height: 80))
     let bottomPlane = SKShapeNode(rectOf: CGSize(width: 1000, height: 10))
-    let spike = SKShapeNode(rectOf: CGSize(width: 50, height: 50))
+    let spike = SKShapeNode(rectOf: CGSize(width: 10, height: 10))
     let rightButton = Button(texture: nil, color: .purple, size: CGSize(width: 100, height: 80))
     let jumpButton = Button(texture: nil, color: .green, size: CGSize(width: 100, height: 80))
     var timer = DispatchTime.now()
-    var resultTimer = DispatchTime.now()
+    var result = 0.0
     var jumped = false
     override func update(_ currentTime: TimeInterval) {
+        result = currentTime
         if (leftButton.isPressed)
         {
             moveBall(left: true)
@@ -34,7 +37,14 @@ class GameScene: SKScene, ButtonDelegate, SKPhysicsContactDelegate {
         {
             moveBall(left: false)
         }
+       
+//        if (currentTime.truncatingRemainder(dividingBy: 10) < 0.1)
+//        {
+//            print("reset")
+//            resetCurve()
+//        }
         //collluiison-detetion-in-spritekit stackoverflow
+        moveSpikes()
     }
     
     func didBegin(_ contact: SKPhysicsContact)
@@ -43,12 +53,16 @@ class GameScene: SKScene, ButtonDelegate, SKPhysicsContactDelegate {
             contact.bodyB.node?.physicsBody?.collisionBitMask == ballCategory)
         {
             print ("Hit!")
+            print(result)
             // restart game here
         }
     }
     
     override func didMove(to view: SKView) {
         physicsWorld.contactDelegate = self
+        let constraintX = SKConstraint.positionX(SKRange.init(lowerLimit: frame.minX, upperLimit: frame.maxX))
+        let constraintY = SKConstraint.positionY(SKRange.init(lowerLimit: frame.minY, upperLimit: frame.maxY))
+        ball.constraints = [constraintX, constraintY]
         ball.name = "ball"
         ball.position = CGPoint(x: view.frame.width / 2, y: view.frame.height / 2)
         ball.physicsBody = SKPhysicsBody(circleOfRadius: 25)
@@ -71,20 +85,29 @@ class GameScene: SKScene, ButtonDelegate, SKPhysicsContactDelegate {
         leftButton.delegate = self
         rightButton.delegate = self
         jumpButton.delegate = self
-        spike.position = CGPoint(x: frame.width/2, y: frame.height * 0.8)
+        spike.position = CGPoint(x: 0, y: frame.height * 0.8)
         spike.fillColor = SKColor.red
-        spike.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        spike.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 10, height: 10))
         spike.physicsBody!.isDynamic = false
         ball.size = CGSize(width: 50, height: 50)
         spike.name = "spike"
         spike.physicsBody!.collisionBitMask = spikeCategory
         spike.physicsBody!.contactTestBitMask = UInt32(7)
+        spikes.append(spike)
+        for i in 0...20 {
+            let newSpike: SKShapeNode = spike.copy() as! SKShapeNode
+            newSpike.position = CGPoint(x: spike.position.x + CGFloat(i * 20), y: spike.position.y + abs(20 * sin(position.x + CGFloat(i * 10))))
+            print(newSpike.position.y)
+            spikes.append(newSpike)
+        }
         addChild(rightButton)
         addChild(ball)
         addChild(leftButton)
         addChild(bottomPlane)
         addChild(jumpButton)
-        addChild(spike)
+        for s in spikes {
+            addChild(s)
+        }
     }
 
     func buttonClicked(sender: Button) {
@@ -119,5 +142,36 @@ class GameScene: SKScene, ButtonDelegate, SKPhysicsContactDelegate {
     func jump()
     {
         ball.physicsBody!.applyImpulse(CGVector(dx: 0, dy: 40))
+    }
+    
+    func moveSpikes()
+    {
+        
+        if (goUp)
+        {
+            for spike in spikes {
+                spike.position.y = spike.position.y + (1.0 + speedFactor)
+            }
+        } else {
+            for spike in spikes {
+                spike.position.y = spike.position.y - (1.0 + speedFactor)
+            }
+        }
+        let currentY = spikes[0].position.y
+        if (currentY >= frame.height)
+        {
+            goUp = false
+        }
+        else if (currentY <= 100)
+        {
+            goUp = true
+        }
+        
+    }
+    func resetCurve(){
+        speedFactor = speedFactor + 0.1
+        for spike in spikes {
+            spike.position.y = frame.maxY - 10
+        }
     }
 }
